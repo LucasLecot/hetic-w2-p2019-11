1c60118bd57bc51684aaaf8dd5db4464ef0f2dd5
marie <marie-charles@sfr.fr>
2018-01-25 23:44:43 +0100

    restructuration architecture selon modèle de B. CALOU

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,82 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
+var CleanWebpackPlugin = require('clean-webpack-plugin');
+var UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+var webpack = require('webpack')
+var path = require('path')
+
+var isProd = process.env.NODE_ENV === 'production';
+var cssDev = ['style-loader', 'css-loader', 'sass-loader']
+var cssProd = ExtractTextPlugin.extract({
+  fallback: 'style-loader',
+  use: ['css-loader', 'sass-loader']
+});
+var cssConfig = isProd ? cssProd : cssDev;
+
 module.exports = {
-  entry: "./app/scripts/main.js",
+  entry: {
+    'index': './app/scripts/index.js'
+  },
+  devtool: isProd ? '' : 'eval-source-map',
   output: {
-    path: __dirname + "./dist/scripts",
-    publicPath: "/scripts",
-    filename: "bundle.js"
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js'
   },
   module: {
     rules: [
       {
         test: /\.scss$/,
-        use: [
-          "style-loader",
-          "css-loader",
-          {
-            loader: "sass-loader",
-            options: {
-              includePaths: ["./app/scss/main.scss", "./dist/css/main.css"]  
-            }	
+        use: cssConfig
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015']
           }
+        }
+      },
+      {
+        test: /\.pug$/,
+        use: 'pug-loader'
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.(gif|png|jpg|svg)$/i,
+        use: [
+          'file-loader',
+          'image-webpack-loader'
         ]
       }
     ]
-  }
-};
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    hot: true,
+    open: true
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Index',
+      template: './app/index.pug',
+      chunks: ['index']
+    }),
+    new ExtractTextPlugin({
+      filename: 'styles.css',
+      disable: !isProd,
+      allChunks: true
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
+    new UglifyJSPlugin()
+  ]
+}
