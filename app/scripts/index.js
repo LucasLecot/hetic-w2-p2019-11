a9ed105109abf002ff6369c5b83be09997a66806
mona <mona.calibani@hetic.net>
2018-02-06 19:51:20 +0100

    counter

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -24,3 +24,59 @@ function moveScreen(event) {
     insideCarView.style.backgroundPosition = newvalueX + "px " + newvalueY + "px";
 }
 
+var percent = 0;
+var test = document.querySelectorAll('.progress-circle .value-bar');
+
+var circleInterval = setInterval(progressCircle, 20);
+
+function progressCircle () {
+    for (var j = 0; j < test.length; j++) {
+        test[j].style.transform = 'rotate('+percent+'deg)';
+        percent+=2;
+        if (percent > 280) {
+            clearInterval(circleInterval);
+        } else if (percent > 180) {
+            var progress = document.querySelectorAll('.progress-circle');
+            for ( let i=0; i < progress.length ; i++ ) {
+                progress[i].classList.add('over50');
+            }
+        }
+    }
+}
+
+var charge = document.querySelector('.charge');
+var chargeValue = 0;
+var chargeCounter = setInterval(chargeProgress, 8);
+function chargeProgress () {
+    if (chargeValue >= 270) {
+        clearInterval(chargeProgress);
+    } else {
+        chargeValue+=2;
+        charge.innerHTML = chargeValue;
+    }
+}
+
+var vision = document.querySelector('.vision');
+var visionValue = 0;
+var visionCounter = setInterval(visionProgress, 5);
+function visionProgress () {
+    if (visionValue >= 2040) {
+        clearInterval(visionProgress);
+    } else {
+        visionValue+=10;
+        vision.innerHTML = visionValue;
+    }
+}
+
+var km = document.querySelector('.km');
+var kmValue = 0;
+var kmCounter = setInterval(kmProgress, 10);
+function kmProgress () {
+    if (kmValue >= 500) {
+        clearInterval(kmProgress);
+    } else {
+        kmValue+=5;
+        km.innerHTML = kmValue;
+    }
+}
+
